7066838f06fee42555080ad53a28ab0af82070c2
pedrohsa <pedroholanda94@gmail.com>
2022-12-11 12:47:14 -0300

    feat: get games route with query string

--- src/controllers/getGames.route.js
+++ src/controllers/getGames.route.js
@@ -1,9 +1,20 @@
 import { db } from "../database/connectPG.js";
 
 const getAllGames = async (req, res) => {
+  const searchFilter = req.query.name;
+  console.log(searchFilter);
+
   try {
-    const categories = await db.query("SELECT * FROM games");
-    res.send(categories.rows);
+    if (searchFilter) {
+      const games = await db.query(
+        "SELECT * FROM games where LOWER(name) LIKE LOWER($1);",
+        [`${searchFilter}%`]
+      );
+      return res.send(games.rows);
+    }
+
+    const games = await db.query("SELECT * FROM games");
+    res.send(games.rows);
   } catch (error) {
     res.status(500).send(error);
   }
